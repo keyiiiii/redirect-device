8684f5392b905634d5de85d41a27b260dfdbb974
板橋 清信 <itahashi_kiyonobu@cyberagent.co.jp>
2018-06-14 16:18:26 +0900

    add other query

--- src/app.js
+++ src/app.js
@@ -3,15 +3,27 @@ import queryString from 'query-string';
 
 const getUA = ua();
 const parsed = queryString.parse(location.search);
+const IOS = 'ios';
+const ANDROID = 'android';
+
+if (!(parsed[IOS] && parsed[ANDROID])) {
+  return false;
+}
+
+const other = {};
+Object.keys(parsed)
+  .filter((key) => !(key === IOS || key === ANDROID))
+  .forEach((key) => other[key] = parsed[key]);
 
 const currentDeviceUA = getUA.os.name;
 const { ios, android } = parsed;
+const otherQuery = queryString.stringify(other);
 
 switch (currentDeviceUA.toLowerCase()) {
-  case 'ios':
-    location.href = ios;
+  case IOS:
+    location.href = `${ios}&${otherQuery}`;
     break;
-  case 'android':
-    location.href = android;
+  case ANDROID:
+    location.href = `${android}&${otherQuery}`;
     break;
 }
